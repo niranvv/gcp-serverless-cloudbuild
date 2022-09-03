4c59929587c13a18afe97c756c27da8ea5a8d7d5
Niran <niranvv@gmail.com>
2022-09-03 07:48:56 +0000

    Test CI CD Checkin

--- node-hello-world/app.js
+++ node-hello-world/app.js
@@ -5,7 +5,7 @@ const express = require('express');
 const app = express();
 
 app.get('/', (req, res) => {
-  res.status(200).send('Hello, world!').end();
+  res.status(200).send('Hello, world - CICD!').end();
 });
 
 // Start the server
